1a66a8629fca8d1da889809c5a5cd0559e8a2d81
James Gerity <snoopjedi@gmail.com>
2025-08-07 00:46:11 -0400

    Add cache root

--- src/kinopy/datamodel/__init__.py
+++ src/kinopy/datamodel/__init__.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+
 from .showingcalendar import ShowingCalendar
 from .showing import Showing
 from .types_ import Day, Cinema
+
+
+CACHE_ROOT = Path().joinpath("kinopy_cache")
+CACHE_ROOT.mkdir(exist_ok=True, parents=True)
